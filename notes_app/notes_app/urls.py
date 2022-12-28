25c01638c830617849b21c920adaed0358077bdf
Olha Maletych <ola.maletych@gmail.com>
2022-12-28 20:40:16 +0200

    First view Olha

--- notes_app/notes_app/urls.py
+++ notes_app/notes_app/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from notes.views import index
-
+from notes_olha.views import hello
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('notes/', index),
+    path('hello_from_Olha/', hello),
 ]
+
