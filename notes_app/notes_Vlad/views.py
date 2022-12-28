1623f9421817ec2ffe51ac8090d573e6db742488
Vladyslav <sharpilo.vladislav96@gmail.com>
2022-12-28 20:46:31 +0200

    Vlad. Add func say_hallo_2

--- notes_app/notes_Vlad/views.py
+++ notes_app/notes_Vlad/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse('Hello from Notes app. Vlad')
+    return HttpResponse('Hello from Notes app. Vlad!')
