363878e22438270cacdb93a167186f3bfe6e2ed2
Vladyslav <sharpilo.vladislav96@gmail.com>
2023-01-06 08:01:14 +0200

    Vlad. На этом этапе все работает, реализован функционал кнопки "отправить"

--- notes_app/notes_Vlad/views.py
+++ notes_app/notes_Vlad/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from .models import NoteForm
 from django.contrib import messages
 from django.urls import reverse
+from django.shortcuts import render, get_object_or_404, redirect
 
 
 class NotesVlad(TemplateView):
